c879f9824775df53b4499db61de218b7d70f7110
Aishwarya Srinivasan <ash.srini89@gmail.com>
2016-09-20 00:29:25 +0530

    Binary Tree problems

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -20,9 +20,8 @@ class Node
     }
 }
 
-public class BinaryTree
-{
-    static Node root;
+public class BinaryTree  {
+     Node root;
 
     public BinaryTree()
     {
@@ -30,7 +29,7 @@ public class BinaryTree
     }
 
     //inorder traversal - left -> root -> right
-    public static void printInorder(Node node)
+    public void printInorder(Node node)
     {
         if(node == null)
             return;
@@ -41,7 +40,7 @@ public class BinaryTree
     }
 
     //preorder traversal - root -> left -> right
-    public static void printPreorder(Node node)
+    public void printPreorder(Node node)
     {
         if (node == null)
             return;
@@ -52,7 +51,7 @@ public class BinaryTree
     }
 
     //post order traversal - left -> right -> root
-    public static void printPostorder(Node node)
+    public void printPostorder(Node node)
     {
         if (node == null)
             return;
@@ -72,7 +71,7 @@ public class BinaryTree
     }
 
     //height of tree
-    public static int height(Node node)
+    public int height(Node node)
     {
         if(node == null)
             return 0;
@@ -84,7 +83,7 @@ public class BinaryTree
     }
     /* level order traversal using recursion and printing given level first
     * worst case complexity is O(n^2) */
-    public static void printLevelOrder()
+    public void printLevelOrder()
     {
         for(int i=1; i<height(root); i++)
         {
@@ -94,7 +93,7 @@ public class BinaryTree
     }
 
     //print given level
-    public static void printGivenLevel(Node node, int level)
+    public void printGivenLevel(Node node, int level)
     {
         if(node == null)
             return;
@@ -102,17 +101,17 @@ public class BinaryTree
             System.out.print(node.key + " ");
         else if(level >1)
         {
-            printGivenLevel(node.left, level--);
-            printGivenLevel(node.right, level--);
+            printGivenLevel(node.left, level-1);
+            printGivenLevel(node.right, level-1);
         }
     }
 
     /* level order traversal using queues
     * complexity is O(n)*/
-    public static void bradthFirstTraversal()
+    public void bradthFirstTraversal(Node node)
     {
         Queue<Node> queue = new LinkedList<Node>();
-        queue.add(root);
+        queue.add(node);
         while(!queue.isEmpty())
         {
             Node temp = queue.poll();
@@ -125,11 +124,76 @@ public class BinaryTree
 
     }
 
-    /* check if two trees are identical */
+    /* check if two trees are identical
+    * Two trees are identical when the number of nodes and the keys of each node is the same*/
+    public boolean identicalTrees(Node node1, Node node2)
+    {
+        if(node1 == null && node2 == null)
+            return true;
+        else if ((node1 != null && node2 != null))
+            return ((node1.key == node2.key) && identicalTrees(node1.left, node2.left) && identicalTrees(node1.right, node2.right));
+        else
+            return false;
+    }
+
 
     /* check if binary tree is balanced*/
+    public int balanceTreeCheck(Node node)
+    {
+        if(node == null)
+            return 0;
 
-    /* conrtruct tree when inprder and post order is given*/
+        int left = balanceTreeCheck(node.left);
+        if (left == -1)
+            return -1;
+
+        int right = balanceTreeCheck(node.right);
+        if (right == -1)
+            return -1;
+
+        if(Math.abs(left-right) > 1 )
+            return -1;
+
+        return Math.max(left,right)+1;
+
+    }
+
+    public boolean isTreeBalanced(Node root){
+        int result = balanceTreeCheck(root);
+        return (result > 0);
+    }
+
+    /* construct tree when inorder and post order is given*/
+    public Node constructFromInorderAndPostorder(int[] inorder, int inorderStart, int inorderEnd, int[] postorder, int postorderStart, int postorderEnd ){
+        if(inorderStart > inorderEnd || postorderStart > postorderEnd)
+            return null;
+        int root = postorder[postorderEnd];
+        Node node = new Node(root);
+
+//        if(inorderStart == inorderEnd)
+//            return node;
+
+        int root_index = 0;
+        for(int i=0; i<inorder.length; i++) {
+            if (inorder[i] == root) {
+                root_index = i;
+                break;
+            }
+
+        }
+        node.left = constructFromInorderAndPostorder(inorder, inorderStart, root_index-1, postorder, postorderStart, postorderStart + root_index - (inorderStart + 1));
+        node.right = constructFromInorderAndPostorder(inorder, root_index+1, inorderEnd, postorder, postorderStart+root_index- inorderStart, postorderEnd - 1);
+
+        return node;
+
+    }
+
+    public Node constructFromInorderAndPostorder(int[] inorder, int[] postorder)
+    {
+        return constructFromInorderAndPostorder(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
+    }
+
+
 
     /* check if tree is a subtree of another binary tree */
 
@@ -145,15 +209,74 @@ public class BinaryTree
 
         tree.root.right.left = new Node(6);
 
-        printInorder(tree.root);
+        //print inorder
+        tree.printInorder(tree.root);
         System.out.println();
-        printPreorder(tree.root);
-        System.out.println();
-        printPostorder(tree.root);
-        System.out.println();
-        System.out.println(size(tree.root));
-        System.out.println(height(tree.root));
 
+        //print preorder
+        tree.printPreorder(tree.root);
+        System.out.println();
+
+        //print postorder
+        tree.printPostorder(tree.root);
+        System.out.println();
+
+        //print tree size
+        System.out.println("Size: " + size(tree.root));
+
+        //print tree height
+        System.out.println("Height: " + tree.height(tree.root));
+
+        //print given level
+        tree.printGivenLevel(tree.root, 2);
+        System.out.println();
+
+        //level order traversal - O(n)
+        tree.bradthFirstTraversal(tree.root);
+        System.out.println();
+
+        //print if tree is balanced or not
+        System.out.println("Is tree balanced? " + tree.isTreeBalanced(tree.root));
+
+        //create a new tree
+        BinaryTree tree2 = new BinaryTree();
+        tree2.root = new Node(1);
+        tree2.root.left = new Node(2);
+        tree2.root.right = new Node(3);
+
+        tree2.root.left.left = new Node(4);
+        tree2.root.left.right = new Node(5);
+
+        tree2.root.right.left = new Node(6);
+
+        tree2.printInorder(tree2.root);
+        System.out.println();
+        tree2.printInorder(tree.root);
+        System.out.println();
+
+        //print if tree is identical or not
+        System.out.println("Are the two trees indentical? " + tree.identicalTrees(tree.root, tree2.root));
+
+        //add new node to tree2
+        tree2.root.right.right = new Node(7);
+
+        //print if tree is identical or not
+        tree2.printInorder(tree2.root);
+        System.out.println();
+        tree2.printInorder(tree.root);
+        System.out.println();
+        System.out.println("Are the two trees indentical? " + tree.identicalTrees(tree.root, tree2.root));
+
+        //given inorder and post rder arrays, construct a binary tree
+        int[] inorder = { 4, 2, 5, 1, 6, 3, 7 };
+        int[] postorder = {4, 5, 2, 6, 7, 3, 1 };
+        BinaryTree newTree = new BinaryTree();
+        newTree.root = newTree.constructFromInorderAndPostorder(inorder, postorder);
+        newTree.printInorder(newTree.root);
+        System.out.println();
+        newTree.printPostorder(newTree.root);
+        System.out.println();
+        newTree.printPreorder(newTree.root);
 
     }
 }
